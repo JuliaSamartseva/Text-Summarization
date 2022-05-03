dbfe257845eac4c93723527c3c46a72ea2ca7c18
Julia <yulia.samartseva@gmail.com>
2022-05-03 12:28:04 +0200

    Update cloud function to handle CORS-enabled requests

--- Text-Summarizer/main.py
+++ Text-Summarizer/main.py
@@ -20,6 +20,24 @@ def read_article(file_json):
 
 
 def generate_summary(request):
+    # Set CORS headers for the preflight request
+    if request.method == 'OPTIONS':
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600'
+        }
+
+        return ('', 204, headers)
+
+    # Set CORS headers for the main request
+    headers = {
+        'Access-Control-Allow-Origin': '*'
+    }
+
     summarizer = LsaSummarizer()
     request_json = request.get_json(silent=True)
     sentences = read_article(request_json)
@@ -27,4 +45,4 @@ def generate_summary(request):
 
     summary = summarizer(sentences, 3)
 
-    return json.dumps(summary)
+    return (json.dumps(summary), 200, headers)
