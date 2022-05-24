54b7d76ef631ce51cd190dfb24f246a022e488c0
Julia <yulia.samartseva@gmail.com>
2022-05-24 23:02:19 +0100

    Send form data using chrome messaging

--- Text-Summarizer-Extension/js/content.js
+++ Text-Summarizer-Extension/js/content.js
@@ -7,27 +7,38 @@ function unicodeToChar(text) {
 	      });
 }
 
-// Capture all text
-var textToSend = document.body.innerText;
+chrome.runtime.onMessage.addListener(function(message) {
+    // Capture all text
+    var textToSend = document.body.innerText;
+    var jsonFormData = message.parameter;
+    
+    // Create JSON from the form data and text from the chrome tab to send
+    var json = {};
+    json['text'] = textToSend;
+    json['query'] = jsonFormData['query'];
+    json['number'] = jsonFormData['number'];
+
+    fetch('https://us-central1-query-summarization.cloudfunctions.net/function-3', {
+      method: 'POST',
+      body: json,
+      headers:{
+        'Content-Type': 'application/json'
+      } })
+    .then(data => { return data.json() })
+    .then(res => { 
+        $.each(res, function( index, value ) {
+            // Highlight summary sentences that the algorithm has identified.
+            var source_document = document.body.innerHTML;
+            value = value.replace(/(\s+)/,"(<[^>]+>)*$1(<[^>]+>)*");
+            var pattern = new RegExp("("+value+")", "i");
+            source_document = source_document.replace(pattern, "<mark>$1</mark>");
+            source_document = source_document.replace(/(<mark>[^<>]*)((<[^>]+>)+)([^<>]*<\/mark>)/,"$1</mark>$2<mark>$4");
+            
+            // Replace the updated text with highlights into the source document.
+            document.body.innerHTML = source_document;
+        });
+     })
+    .catch(error => console.error('Error:', error));
+}
 
 
-fetch('https://us-central1-query-summarization.cloudfunctions.net/function-3', {
-  method: 'POST',
-  body: JSON.stringify(textToSend),
-  headers:{
-    'Content-Type': 'application/json'
-  } })
-.then(data => { return data.json() })
-.then(res => { 
-	$.each(res, function( index, value ) {
-        // Highlight summary sentences that the algorithm has identified.
-        var source_document = document.body.innerHTML;
-        value = value.replace(/(\s+)/,"(<[^>]+>)*$1(<[^>]+>)*");
-        var pattern = new RegExp("("+value+")", "i");
-        source_document = source_document.replace(pattern, "<mark>$1</mark>");
-        source_document = source_document.replace(/(<mark>[^<>]*)((<[^>]+>)+)([^<>]*<\/mark>)/,"$1</mark>$2<mark>$4");
-        
-		document.body.innerHTML = source_document;
-	});
- })
-.catch(error => console.error('Error:', error));
--- Text-Summarizer-Extension/js/popup.js
+++ Text-Summarizer-Extension/js/popup.js
@@ -1,4 +1,14 @@
 function summarize() {
+    
+    // Capture the data from the form
+    var query = document.getElementById('query').value;
+    var number = document.getElementById('number').value;
+    
+    // Create JSON from the form data
+    var json = {};
+    json['query'] = query;
+    json['number'] = number;
+    
     chrome.windows.getCurrent(function (currentWindow) {
         chrome.tabs.query({ active: true, windowId: currentWindow.id }, function (activeTabs) {
             activeTabs.map(function (tab) {
@@ -10,12 +20,15 @@ function summarize() {
                     chrome.scripting.executeScript({
                         target: {tabId: tab.id, allFrames: false},
                         files: ['js/content.js'],
+                    }, function () {
+                        chrome.tabs.sendMessage(tab.id, {parameter: json});
                     });
                 });
+                
             });
         });
     });
 }
 
-// Call a script summarization function in the current tab when the button is clicked.
+// Call a script summarization function in the current tab when the button is clicked
 document.getElementById('summarize_text').addEventListener('click', summarize);
